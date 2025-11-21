781afa232dd964cfc756cb283c271c65f2ae64ad
Saytenz <anggiecallme@gmail.com>
2025-11-21 15:44:57 +0700

    merubah isi code di server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const db = require('./db/js');
+const db = require('./db.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { authenticateToken, authorizeRol } = require('./middleware/auth.js');
